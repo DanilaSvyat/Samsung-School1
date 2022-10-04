d0cd96e20e97a316bff2b8a485265606138abe51
DanilaSvyat <Danil_ukr1@mail.ru>
2022-10-04 17:19:59 +0300

    task 4 weakness

--- hw1/src/com/samsung/hw1/Main.java
+++ hw1/src/com/samsung/hw1/Main.java
@@ -6,19 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
 
-        short rub, kol, kop, resrub, reskop;
+        int rub, kol, kop, resrub, reskop;
         double sum, res, h;
         rub = a.nextShort();
         kop = a.nextShort();
         kol = a.nextShort();
-        sum = ((double) kop / 100) + rub;
-        res = sum * kol;
-        resrub = (short) res;
-        h = res * 1000;
-        reskop = 0;
-        reskop += h % 100;
-        h /= 10;
-        reskop += h % 100;
-        System.out.println(resrub + " " + reskop);
+        rub = (rub  * kol) + (kop * kol / 100);
+        kop = (kop * kol) % 100;
+
+        System.out.println(rub + " " + kop);
     }
 }
