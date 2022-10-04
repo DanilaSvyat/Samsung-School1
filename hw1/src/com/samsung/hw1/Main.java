29aeb16ede17c21bfbe6c2f102a598abdb1f0425
DanilaSvyat <Danil_ukr1@mail.ru>
2022-10-04 12:54:11 +0300

    task 5

--- hw1/src/com/samsung/hw1/Main.java
+++ hw1/src/com/samsung/hw1/Main.java
@@ -5,21 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        Scanner b = new Scanner(System.in);
-        Scanner n = new Scanner(System.in);
-        short rub, kol, kop, resrub, reskop;
-        double sum, res, h;
-        rub = a.nextShort();
-        kop = b.nextShort();
-        kol = n.nextShort();
-        sum = ((double) kop / 100) + rub;
-        res = sum * kol;
-        resrub = (short) res;
-        h = res * 100;
-        reskop = 0;
-        reskop += h % 10;
-        h /= 10;
-        reskop += h % 10;
-        System.out.println(resrub + " " + reskop);
+
+        int n, hours, min, sec;
+        n = a.nextShort();
+        hours = n / 3600;
+        n = n - (3600 * hours);
+        min = n / 60;
+        n = n - (60 * min);
+        sec = n;
+        System.out.println(hours + ":" + min + ":" + sec);
     }
 }
