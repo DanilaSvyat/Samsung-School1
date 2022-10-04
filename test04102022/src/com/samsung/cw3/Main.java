a082cf694fc4fc06b70729712d61855112cf1d6e
DanilaSvyat <Danil_ukr1@mail.ru>
2022-10-04 20:55:49 +0300

    class work

---
+++ test04102022/src/com/samsung/cw3/Main.java
@@ -0,0 +1,19 @@
+package com.samsung.cw3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+        String res = (y > 0 && x * x + y * y > 4 && x < 2 && y < x ) ? "YES" : "NO";
+        System.out.println(res);
+
+
+
+    }
+}
