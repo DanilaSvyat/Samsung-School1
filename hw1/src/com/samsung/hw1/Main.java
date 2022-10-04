e38bbfafa398a7e59df74ab972c0ec03165cd0c8
DanilaSvyat <Danil_ukr1@mail.ru>
2022-10-04 17:05:28 +0300

    task 4 second try

--- hw1/src/com/samsung/hw1/Main.java
+++ hw1/src/com/samsung/hw1/Main.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
 
-        Scanner x = new Scanner(System.in);
-        Scanner y = new Scanner(System.in);
-        short a, b;
-        int res;
-        a = x.nextShort();
-        b = y.nextShort();
-
-        res = a * a + b * b;
-
-        System.out.println(res);
-
+        short rub, kol, kop, resrub, reskop;
+        double sum, res, h;
+        rub = a.nextShort();
+        kop = a.nextShort();
+        kol = a.nextShort();
+        sum = ((double) kop / 100) + rub;
+        res = sum * kol;
+        resrub = (short) res;
+        h = res * 1000;
+        reskop = 0;
+        reskop += h % 100;
+        h /= 10;
+        reskop += h % 100;
+        System.out.println(resrub + " " + reskop);
     }
 }
