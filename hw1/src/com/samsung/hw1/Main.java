19131894f6e0ecb6c45a17a6c5342eb2d0817c6c
DanilaSvyat <Danil_ukr1@mail.ru>
2022-10-04 13:06:57 +0300

    add.task 4

--- hw1/src/com/samsung/hw1/Main.java
+++ hw1/src/com/samsung/hw1/Main.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
 
-        int n, hours, min, sec;
-        n = a.nextShort();
-        hours = n / 3600;
-        n = n - (3600 * hours);
-        min = n / 60;
-        n = n - (60 * min);
-        sec = n;
-        System.out.println(hours + ":" + min + ":" + sec);
+        Scanner x = new Scanner(System.in);
+        Scanner y = new Scanner(System.in);
+        short a, b;
+        int res;
+        a = x.nextShort();
+        b = y.nextShort();
+
+        res = a * a + b * b;
+
+        System.out.println(res);
+
     }
 }
