3a60529e29b12937bce10c580d31c24c3cca4ff4
DanilaSvyat <Danil_ukr1@mail.ru>
2022-10-05 12:03:14 +0300

    task 5

--- hw2/src/com/samsung/hw2/Main.java
+++ hw2/src/com/samsung/hw2/Main.java
@@ -6,9 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int x = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
 
-        boolean res = x / 100 > 0 && x / 100 < 9 && x % 5 == 0;
+        boolean res = a == -b || a == -c || a == -d || b == -c || b == -d || c == - d;
         System.out.println(res);
     }
 }
