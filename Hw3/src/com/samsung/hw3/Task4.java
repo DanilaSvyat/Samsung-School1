21969cad10308c73638e86bead125e4a92062688
DanilaSvyat <Danil_ukr1@mail.ru>
2022-10-11 00:57:43 +0300

    all without rome1

--- Hw3/src/com/samsung/hw3/Task4.java
+++ Hw3/src/com/samsung/hw3/Task4.java
@@ -8,7 +8,7 @@ public class Task4 {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
-
+        double c = b / a;
         if (a <= 0 && b <= 0) {
             System.out.println("no such x");
         } else {
@@ -17,10 +17,10 @@ public class Task4 {
             }
         }
         if (a > 0 && b < 0) {
-            System.out.println("x<" + b / a + " or x>" + -b / a);
+            System.out.printf("x<%.1f or x>%.1f", c, -c);
         }
         if (a < 0 && b > 0) {
-            System.out.println(b / a + "<x<" + -b / a);
+            System.out.printf("%.1f<x<%.1f", c, -c);
         }
     }
 }
