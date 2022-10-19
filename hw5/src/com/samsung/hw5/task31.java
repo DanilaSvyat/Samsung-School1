b2bbaaed8275e4dfb4368f13dcd8a8759f6dacc1
DanilaSvyat <Danil_ukr1@mail.ru>
2022-10-20 00:05:02 +0300

    task31newaddition

--- hw5/src/com/samsung/hw5/task31.java
+++ hw5/src/com/samsung/hw5/task31.java
@@ -12,17 +12,21 @@ public class task31 {
         }
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        for (int i = a - 1; i <= (b + a - 2) / 2; i++) {
-            int c = arr[i];
-            arr[i] = arr[b - i + a - 2];
-            arr[b - i + a - 2] = c;
+        for (int i = a-1; i <= b-1; i++) {
+            int c = arr[a-1];
+            arr[a-1] = arr[b-1];
+            arr[b-1] = c;
+            a++;
+            b--;
         }
         int c = scanner.nextInt();
         int d = scanner.nextInt();
-        for (int i = c - 1; i <= (d + c - 2) / 2; i++) {
-            int e = arr[i];
-            arr[i] = arr[d - i + c - 2];
-            arr[d - i + c - 2] = e;
+        for (int i = c-1; i <= d-1; i++) {
+            int e = arr[c-1];
+            arr[c-1] = arr[d-1];
+            arr[d-1] = e;
+            c++;
+            d--;
         }
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
