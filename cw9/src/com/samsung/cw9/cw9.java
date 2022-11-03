3a9dfc50fbee3df2bf651dd9e83c576893602755
DanilaSvyat <Danil_ukr1@mail.ru>
2022-11-03 19:23:35 +0300

    cw9

---
+++ cw9/src/com/samsung/cw9/cw9.java
@@ -0,0 +1,20 @@
+package com.samsung.cw9;
+
+import com.samsung.cw9.model.Person;
+
+public class cw9 {
+    public static void main(String[] args) {
+
+        Person person = new Person("Valera", 18);
+        Person person1 = new Person("Kolya", 14);
+
+
+person.birthday();
+person.printInfo();
+
+Integer a = 1;
+
+a.toString();
+
+    }
+}
