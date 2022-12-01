9255d6210cde388361607a70477c2f28686ff71e
DanilaSvyat <Danil_ukr1@mail.ru>
2022-12-01 19:25:13 +0300

    ItakSoidet+++

--- cw17exam/src/com/samsung/model/DemoService.java
+++ cw17exam/src/com/samsung/model/DemoService.java
@@ -11,12 +11,10 @@ public class DemoService {
         Student student2 = new Student("Vitalii","Ivanov","Valerievich",
                 "+79123123123","10A");
         Teacher teacher1 = new Teacher("Valeriy","Ivanov","Gavrilovich","Math");
-        University university = new University(student1,student2,teacher1);
+        Student[] ad = {student1,student2};
+        Teacher[] af = {teacher1};
+        University university = new University(ad,af);
         System.out.println(university.printInfoToConsole());
-        System.out.println("Или так:" + "\n");
-        university.newStudent(student1);
-        university.newStudent(student2);
-        university.newTeacher(teacher1);
-        System.out.println(university.printInfoToConsole());
+
     }
 }
--- cw17exam/src/com/samsung/model/University.java
+++ cw17exam/src/com/samsung/model/University.java
@@ -4,50 +4,26 @@ import java.util.Arrays;
 
 public class University implements Printer {
     private final String address = "Москва, Улица ленина.";
-    static int studentCount;
-    static int teacherCount;
-    private String[] studentsList;
-    private String[] teachersList;
 
+    private Student[] studentsList;
+    private Teacher[] teachersList;
 
-
-
-    public University(Student student,Student student1,Teacher teacher1) {
-        studentsList = new String[2];
-        teachersList = new String[1];
-
-            studentsList[0] = student.printInfoToConsole();
-            studentsList[1] = student1.printInfoToConsole();
-            teachersList[0] = teacher1.printInfoToConsole();
+    public University(Student[] studentsList, Teacher[] teachersList) {
+        this.studentsList = studentsList;
+        this.teachersList = teachersList;
     }
-    public University(){
-        studentsList = new String[1000];
-        teachersList = new String[50];
-    }
-    public String[] newStudent(Student student){
-        studentsList[studentCount] = student.printInfoToConsole();
-        studentCount++;
-        return studentsList;
-    }
-    public String[] newTeacher(Teacher teacher){
-        studentsList[teacherCount] = teacher.printInfoToConsole();
-        teacherCount++;
-        return studentsList;
-    }
-
-
-    /*public University(Teacher teacher) {
-        studentsList = new String[1000];
-        while (i < 1001) {
-            studentsList[1] = teacher.printInfoToConsole();
-        }
-        i++;
-    }*/
 
     @Override
     public String printInfoToConsole() {
+        return (this).toString();
+    }
+
+    @Override
+    public String toString() {
         return "Адресс университета:" + this.address + "\n" +
                 "Список учителей: " + Arrays.toString(this.teachersList) + "\n" +
                 "Список учеников: " + Arrays.toString(this.studentsList);
     }
+
+
 }
