f7d4119c63931ed2d3a29e31a793cd2cede184d0
DanilaSvyat <Danil_ukr1@mail.ru>
2023-02-13 23:44:16 +0300

    LinkedLists

---
+++ test130223/src/com/samsung/App.java
@@ -0,0 +1,44 @@
+package com.samsung;
+
+import com.samsung.lesson.MyLinkedList;
+import com.samsung.person.Person;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        List<Person> personList = new LinkedList<>();
+
+
+/*
+        MyLinkedList<Person> personMyLinkedList = new MyLinkedList<>();
+
+        personMyLinkedList.add(new Person("Valera", 13));
+        Person kolya = new Person("Kolya", 18);
+        personMyLinkedList.add(kolya);
+        personMyLinkedList.add(new Person("Sasha1", 21));
+        personMyLinkedList.add(new Person("Sasha2", 21));
+        personMyLinkedList.add(new Person("Sasha3", 21));
+
+
+        for (Object o : personMyLinkedList) {
+            System.out.println(o);
+        }
+
+        personMyLinkedList.remove(kolya);
+
+        for (Object o : personMyLinkedList) {
+            System.out.println(o);
+        }
+        System.out.println("===========");
+        System.out.println(personMyLinkedList.get(3));
+
+        */
+
+    }
+
+
+}
